3951df6dd9e26bed65004a4cbd309b840aeb98f1
Ricardo Vieira <ricardo.vieira@tecnico.ulisboa.pt>
2016-03-20 15:56:22 +0000

    Add argparse to readsensors so we can choose the channel to read
    Also update systemd timer and service to select the channel

--- raspberry/readsensors.py
+++ raspberry/readsensors.py
@@ -2,10 +2,18 @@
 
 import serial
 import time, json
+import argparse
 
-DATA_PATH = "/srv/http"
+parser = argparse.ArgumentParser(description='Aquire sensor data from a given channel')
+parser.add_argument('-c', dest='channel', metavar='channel', type=int, required=True,
+                    help='Channel to be aquired.')
+parser.add_argument('--data', dest='data_path', default="/srv/http/", type=str,
+                    help='The directory where the files will be written')
+args = parser.parse_args()
+
+DATA_PATH = args.data_path
 NSAMPLES = 128
-CHANNEL = "3"
+CHANNEL = args.channel
 
 try:
     port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
@@ -14,7 +22,7 @@ except serial.SerialException as e:
 
 port.xonxoff = False
 
-port.write(('\rAQUIRE ' + CHANNEL + '\r\n').encode('ascii'))
+port.write(('\rAQUIRE ' + str(CHANNEL) + '\r\n').encode('ascii'))
 
 rcv = port.readline()
 line = rcv.decode('ascii')
@@ -40,7 +48,7 @@ while "current_rms" not in line:
 
 current_rms = float(line[12:])
 
-output_file = open(DATA_PATH + '/sensor_data_' + CHANNEL + '.json', 'w+')
+output_file = open(DATA_PATH + '/sensor_data_' + str(CHANNEL) + '.json', 'w+')
 output_file.write(json.dumps({"voltage": voltages,
                               "current": currents,
                               "voltage_rms": voltage_rms,
@@ -48,6 +56,6 @@ output_file.write(json.dumps({"voltage": voltages,
                               "date": time.asctime()}, indent=4))
 output_file.close()
 
-power_fd = open(DATA_PATH + "/rms_values_" + CHANNEL + ".csv", 'a+')
+power_fd = open(DATA_PATH + "/rms_values_" + str(CHANNEL) + ".csv", 'a+')
 power_fd.write(str(time.time()) + ", " + str(voltage_rms) + ", " + str(current_rms) + "\n")
 power_fd.close()
