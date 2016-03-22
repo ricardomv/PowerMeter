79e75b39b5989344c135f23db6789caed4a4aed0
Ricardo Vieira <ricardo.vieira@tecnico.ulisboa.pt>
2016-03-22 21:48:30 +0000

    Added gauges with last acquired RMS values

--- raspberry/web/js/index.js
+++ raspberry/web/js/index.js
@@ -3,6 +3,8 @@ var m_channel = 0;
 function init() {
     document.getElementById("hostname").innerHTML = location.hostname;
     reload_data();
+    createGauge("voltage", "Voltage", 0, 300);
+    createGauge("current", "Current", 0, 30);
 }
 function change_channel(channel) {
     m_channel = channel;
@@ -16,6 +18,11 @@ function reload_data() {
         if (request_inst.status === 404) {
             Plotly.deleteTraces('inst_voltage', 0);
             Plotly.deleteTraces('inst_current', 0);
+            document.getElementById("voltage_rms").innerHTML = "Vrms: 0";
+            document.getElementById("current_rms").innerHTML = "Irms: 0";
+            document.getElementById("aq_date").innerHTML = "";
+            gauges["voltage"].redraw(0);
+            gauges["current"].redraw(0);
             return;
         }
         var DATA = JSON.parse(request_inst.responseText);
@@ -25,9 +32,9 @@ function reload_data() {
         Plotly.newPlot('inst_current', [{
                        y: DATA["current"] }], {
                        margin: { t: 0 } } );
-        document.getElementById("voltage_rms").innerHTML = "Vrms: " + DATA["voltage_rms"];
-        document.getElementById("current_rms").innerHTML = "Irms: " + DATA["current_rms"];
         document.getElementById("aq_date").innerHTML = DATA["date"];
+        gauges["voltage"].redraw(DATA["voltage_rms"]);
+        gauges["current"].redraw(DATA["current_rms"]);
     }
     request_inst.send(null);
 
@@ -51,12 +58,41 @@ function reload_data() {
             time.setTime(values[0]*1000)
             date.push(time)
         }
+        var layout = {
+
+        };
         Plotly.newPlot( 'power', [{
                 y: power,
-                x: date}], {
-                margin: { t: 0 } } );
+                x: date}],
+                {
+                    margin: { t: 0 },
+                    yaxis: {
+                        rangemode: 'tozero',
+                        autorange: true
+                    }
+                } );
         document.getElementById("download_csv_file").href = "rms_values_" + m_channel +".csv";
     }
     request_rms.send(null);
 }
 var m_interval = setInterval(reload_data, 60 * 1000, m_channel);
+var gauges = [];
+
+function createGauge(name, label, min, max)
+{
+  	var config =
+  	{
+    		size: 150,
+    		label: label,
+    		min: undefined != min ? min : 0,
+    		max: undefined != max ? max : 100,
+    		minorTicks: 5
+  	}
+
+  	var range = config.max - config.min;
+  	config.yellowZones = [{ from: config.min + range*0.75, to: config.min + range*0.9 }];
+  	config.redZones = [{ from: config.min + range*0.9, to: config.max }];
+
+  	gauges[name] = new Gauge(name + "GaugeContainer", config);
+  	gauges[name].render();
+}
